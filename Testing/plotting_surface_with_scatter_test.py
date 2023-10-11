95fd8bafc5f08b566d52a010a442159e52d22593
thoang891 <thoang891@gmail.com>
2023-10-10 23:32:47 -0400

    Succesful surface/scatter plotting
    See test file

--- Testing/plotting_surface_with_scatter_test.py
+++ Testing/plotting_surface_with_scatter_test.py
@@ -1,34 +1,35 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 import time
+import random
 #from mpl_toolkits import mplot3d
 
-x = np.outer(np.linspace(-10, 10, 1000), np.ones(1000))
+x = np.outer(np.linspace(-20, 20, 1000), np.ones(1000))
 y = x.copy().T
-z = np.sinh(x) * -1 * np.sinh(y)
+z = np.sinh(x/5) * np.sin(y/5) * (x**2 + y**2)
 
 fig = plt.figure(figsize=(14,9))
 ax = plt.axes(projection='3d')
 
-ax.plot_surface(x, y, z)
+ax.plot_surface(x, y, z, cmap='viridis', alpha=0.5)
 
 u = 0
 v = 0
 
 scatter_plot = None
 
-for i in range(10):
-    u += 1
-    v += 1
-    w = np.sinh(u) * -1 * np.sinh(v)
+for i in range(1000):
+    u += random.uniform(-1,1)
+    v += random.uniform(-1,1)
+    w = np.sinh(u/5) * np.sin(v/5) * (u**2 + v**2)
 
-    scatter_plot = ax.scatter(u, v, w, c='r', marker='o')
+    scatter_plot = ax.scatter(u, v, w, c='r', s=50, marker='o', depthshade=False)
     
-    plt.pause(1)
+    plt.pause(0.001)
     
     scatter_plot.remove()
     
-    time.sleep(0.1)
+    #time.sleep(0.01)
 
 plt.show()
 
