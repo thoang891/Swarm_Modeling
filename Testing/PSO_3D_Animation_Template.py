53e1fc15bb35dc390453bf16d04d463d8823ddb8
Thinh Hoang <thoang891@gmail.com>
2023-10-11 17:01:02 -0400

    Succesful template of 3D animation with PSO algorithm generated

---
+++ Testing/PSO_3D_Animation_Template.py
@@ -0,0 +1,82 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import pyswarms as ps
+
+# Surface Data set
+x_space = np.outer(np.linspace(-10, 10, 2000), np.ones(2000))
+y_space = x_space.copy().T
+
+# Objective Function
+def fx(x, y):
+    return (np.sinc(x) + np.sinc(y) + (np.cos(x * y) / 10))
+
+#Obectjive Function for Swarm
+
+def objective_func(x): # Known solution at position (0,0) and value of 2
+    x_ = x[:, 0]
+    y_ = x[:, 1]
+    z = fx(x_, y_)
+    return z
+
+# Optimize
+options = {'c1': 0.5, 'c2': 0.3, 'w': 0.9}
+n_particles = 25
+dimensions = 2
+iters = 1000
+max_bounds = 10 * np.ones(dimensions)
+min_bounds = -max_bounds
+bounds = (min_bounds, max_bounds)
+optimizer = ps.single.GlobalBestPSO(n_particles=n_particles, dimensions=dimensions, options=options, bounds=bounds)
+cost, pos = optimizer.optimize(objective_func, iters=iters)
+
+# Extract position and cost history
+pos_history = optimizer.pos_history
+pos_history = np.array(pos_history)
+print(pos_history.shape)
+
+cost_history = optimizer.cost_history
+cost_history = np.array(cost_history)
+
+# Initilize empty lists for position values
+
+x_vals = [[] for _ in range(n_particles)]  # Create empty lists for each particle
+y_vals = [[] for _ in range(n_particles)]
+
+# Extract x and y values from position history
+for i in range(iters):
+    for j in range(n_particles):
+        x_pos = pos_history[i, j, 0]
+        y_pos = pos_history[i, j, 1]
+        x_vals[j].append(x_pos)
+        y_vals[j].append(y_pos)
+
+# Plotting and animation
+fig = plt.figure(figsize=(7, 7))
+ax = plt.axes(projection='3d')
+ax.set_xlim(-10, 10)
+ax.set_ylim(-10, 10)
+ax.plot_surface(x_space, y_space, fx(x_space, y_space), cmap='viridis', alpha=0.5)
+
+scatter_plots = []
+delay = 0.07
+
+for i in range(iters):
+    # Clear the previous scatter plots
+    for plot in scatter_plots:
+        plot.remove()
+    scatter_plots = []  # Clear the list for the new iteration
+
+    for particle_index in range(n_particles):
+        x = x_vals[particle_index][i]
+        y = y_vals[particle_index][i]
+        fx(x, y)
+        # z = (np.sinc(x) + np.sinc(y) + (np.cos(x * y) / 10)) * -1
+
+        scatter_plot = ax.scatter(x, y, fx(x, y), c='b', s=50, marker='o', depthshade=False)
+        scatter_plots.append(scatter_plot)
+
+    plt.pause(delay)
+
+plt.show()
--- Testing/plotting_surface_with_scatter_test.py
+++ Testing/plotting_surface_with_scatter_test.py
@@ -1,6 +1,5 @@
 import numpy as np 
 import matplotlib.pyplot as plt
-import time
 import random
 #from mpl_toolkits import mplot3d
 
@@ -9,8 +8,8 @@ y = x.copy().T
 
 fig = plt.figure(figsize=(14, 9))
 ax = plt.axes(projection='3d')
-ax.set_xlim(-20, 20)
-ax.set_ylim(-20, 20)
+ax.set_xlim(-10, 10)
+ax.set_ylim(-10, 10)
 
 u = 0
 v = 0
@@ -18,25 +17,25 @@ v = 0
 scatter_plot = None
 surface_plot = None
 
-for i in range(100):
-    u += random.uniform(-1,1)
-    v += random.uniform(-1,1)
-    w = np.sinc((u + float(i)/10) * v)
+for i in range(1000):
+    u += random.uniform(-0.1,0.1)
+    v += random.uniform(-0.1,0.1)
+    w = np.sinc(u * np.cos(float(i)/100)) + np.sinc(v * np.sin(float(i)/100)) + (np.cos(u * v * np.sin(float(i)/100)) / 10)
 
-    z = np.sinc((x + float(i)/10) * y)
+    z = np.sinc(x * np.cos(float(i)/100)) + np.sinc(y * np.sin(float(i)/100)) + (np.cos(x * y * np.sin(float(i)/100)) / 10)
     print(z)
     surface_plot = ax.plot_surface(x, y, z, cmap='viridis', alpha=0.5)
     scatter_plot = ax.scatter(u, v, w, c='r', s=50, marker='o', depthshade=False)
     
     plt.pause(0.001)
     
-    if scatter_plot is not None and i < 99:
+    if scatter_plot is not None and i < 999:
         scatter_plot.remove()
 
-    if surface_plot is not None and i < 99:
+    if surface_plot is not None and i < 999:
         surface_plot.remove()
     
-    time.sleep(0.01)
+   # time.sleep(0.01)
 
 plt.show()
 
