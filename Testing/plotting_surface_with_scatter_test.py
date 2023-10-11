4ae22565660217f90300de54ebd50c0ec6208d07
Thinh Hoang <thoang891@gmail.com>
2023-10-11 14:21:46 -0400

    Successful animation with dynamic surface plotting

--- Testing/plotting_surface_with_scatter_test.py
+++ Testing/plotting_surface_with_scatter_test.py
@@ -4,32 +4,39 @@ import time
 import random
 #from mpl_toolkits import mplot3d
 
-x = np.outer(np.linspace(-20, 20, 1000), np.ones(1000))
+x = np.outer(np.linspace(-10, 10, 1000), np.ones(1000))
 y = x.copy().T
-z = np.sinh(x/5) * np.sin(y/5) * (x**2 + y**2)
 
-fig = plt.figure(figsize=(14,9))
+fig = plt.figure(figsize=(14, 9))
 ax = plt.axes(projection='3d')
-
-ax.plot_surface(x, y, z, cmap='viridis', alpha=0.5)
+ax.set_xlim(-20, 20)
+ax.set_ylim(-20, 20)
 
 u = 0
 v = 0
 
 scatter_plot = None
+surface_plot = None
 
-for i in range(1000):
+for i in range(100):
     u += random.uniform(-1,1)
     v += random.uniform(-1,1)
-    w = np.sinh(u/5) * np.sin(v/5) * (u**2 + v**2)
+    w = np.sinc((u + float(i)/10) * v)
 
+    z = np.sinc((x + float(i)/10) * y)
+    print(z)
+    surface_plot = ax.plot_surface(x, y, z, cmap='viridis', alpha=0.5)
     scatter_plot = ax.scatter(u, v, w, c='r', s=50, marker='o', depthshade=False)
     
     plt.pause(0.001)
     
-    scatter_plot.remove()
+    if scatter_plot is not None and i < 99:
+        scatter_plot.remove()
+
+    if surface_plot is not None and i < 99:
+        surface_plot.remove()
     
-    #time.sleep(0.01)
+    time.sleep(0.01)
 
 plt.show()
 
