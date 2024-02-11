72c82857ab0d11d0227a197a6c477287d5476700
Thinh Hoang <thoang891@gmail.com>
2024-02-11 14:44:37 -0500

    Initializing isocontour movement behavior development
    Isocontour moving buoys move along where the measure is constant

---
+++ Development/Isocontour_Dev/Swarm.py
@@ -0,0 +1,66 @@
+from Buoy import Buoy
+
+class Swarm():
+
+    def __init__(self, seeker_pop = 2, explorer_pop = 2, com_radius = 7, 
+                 speed=2, timestep = 0.1, map_size = 10):
+        self.seeker_population = seeker_pop
+        self.explorer_population = explorer_pop
+        self.swarm = []
+        self.timestep = timestep
+        self.map_size = map_size
+        self.broadcast_data = []
+        self.com_radius = com_radius
+        self.speed = speed
+
+    def construct(self):
+        for i in range(self.seeker_population):
+            self.swarm.append(Buoy(id=i+1, com_radius=self.com_radius, 
+                                   speed=self.speed, timestep=self.timestep, 
+                                   behv="seeker", bounds=self.map_size))
+        
+        for i in range(self.explorer_population):
+            self.swarm.append(Buoy(id=i+1+self.seeker_population, com_radius=self.com_radius, 
+                                   speed=self.speed, timestep=self.timestep, behv="explorer", bounds=self.map_size))
+
+        for buoy in self.swarm:
+            buoy.measure()
+
+        return self.swarm
+
+    def broadcast(self):
+        broadcast_data = [] # Clear the list for the new iteration
+
+        # Build broadcast data for each buoy from each buoy
+        for buoy in self.swarm:
+            id = buoy.id
+            behavior = buoy.behv
+            x = buoy.position[0]
+            y = buoy.position[1]
+            z = buoy.measure()
+            best_x = buoy.best_known_position[0]
+            best_y = buoy.best_known_position[1]
+            best_measure = buoy.best_known_measure
+            best_id = buoy.best_known_id
+
+            print("ID: {0:>2}, Behavior: {1:8}, Position: {2:>6.2f}, {3:>6.2f}, Measurement: {4:>6.2f}, Best Known Position: {5:>6.2f}, {6:>6.2f}, Best Known Measurement: {7:>6.2f}, Best Known ID: {8:>2}"
+                  .format(id, behavior, x, y, z, best_x, best_y, best_measure, best_id))
+
+            buoy_data = {'ID': id, 'behv': behavior , 'x': x, 'y': y, 'Measurement': z, 
+                         'best_x': best_x, 'best_y': best_y, 'best_measure': best_measure, 'best_id': best_id}
+            broadcast_data.append(buoy_data)
+        
+        print("Broadcast Data")
+        print("\n".join(str(data) for data in broadcast_data)) # Print broadcast data to be read by buoy
+        self.broadcast_data = broadcast_data
+        return self.broadcast_data
+
+    def update(self):
+        # for buoy in self.swarm:
+        #     buoy.measure()
+
+        self.broadcast()
+        for buoy in self.swarm:
+            buoy.read_mail(self.broadcast_data)
+            buoy.update()
+ 
---
+++ Development/Isocontour_Dev/main.py
@@ -0,0 +1,69 @@
+import matplotlib.pyplot as plt
+
+from Environment import Env
+from Swarm import Swarm
+
+# Settings
+timestep = 0.1
+iterations = 1000
+map_size = 10
+seeker_population = 5
+explorer_population = 20
+communication_radius = 10
+speed = 2
+
+# Initialize Environment and Plot
+env = Env(bounds=map_size, fidelity=2000, dt=timestep)
+ax = plt.axes(projection='3d')
+ax.set_xlim(-env.bounds, env.bounds)
+ax.set_ylim(-env.bounds, env.bounds)
+
+def surf_plot():
+    ax.plot_surface(env.x_space, env.y_space, 
+                    env.z_space(), cmap='viridis', alpha=0.5) # Plot the surface
+
+def main(iters=iterations):
+    swarm = Swarm(seeker_pop=seeker_population, explorer_pop=explorer_population, 
+                  com_radius=communication_radius, speed=speed, timestep=timestep, map_size=map_size)
+    swarm.construct()
+    surf_plot()
+
+    scatter_plots = []
+    delay = 0.2
+    total_time = iters*env.dt
+
+    # Animation Loop
+    for i in range(iters):
+        # Clear the previous scatter plots
+        for plot in scatter_plots:
+            plot.remove()
+        scatter_plots = []  # Clear the list for the new iteration
+        broadcast_data = [] # Clear the list for the new iteration
+
+        print("#"*100)
+        print("Iteration: {0} Time Elapsed: {1:>10.2f} seconds/ {2:>10.2f} seconds".format(i, i*env.dt, total_time))
+        print("#"*100)
+
+        swarm.update()
+        broadcast_data = swarm.broadcast_data
+
+        for mail in broadcast_data:
+            id = mail['ID']
+            behavior = mail['behv']
+            x = mail['x']
+            y = mail['y']
+            z = mail['Measurement']
+           
+            if behavior == "seeker":
+                scatter_plot = ax.scatter(x, y, z, c='r', marker='o')
+            else:
+                scatter_plot = ax.scatter(x, y, z, c='b', marker='o')
+            scatter_plots.append(scatter_plot)
+
+        plt.pause(delay)
+        print(" ")
+
+    plt.show()
+
+if __name__ == "__main__":
+    main(iterations)
